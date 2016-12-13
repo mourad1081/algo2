16f6323a0b96ba2fc2836e8865ea64bad2bd986d
mourad1081 <Allah99.>
2016-12-13 13:05:40 +0100

    Adjacence fini

--- Algo2/src/Main.java
+++ Algo2/src/Main.java
@@ -1,5 +1,8 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world !");
+    public static void main(String[] args) throws IOException {
+        Graphe g = new Graphe("grapheText.txt");
+        System.out.println(g.toString());
     }
 }
