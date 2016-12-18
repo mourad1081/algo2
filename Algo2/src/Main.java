0bb7a18617610e381fb0e8c3e872b18b50fdbe19
Mourad <mourad1081@gmail.com>
2016-12-18 12:42:51 +0100

    Plus bel affichage

--- Algo2/src/Main.java
+++ Algo2/src/Main.java
@@ -2,39 +2,54 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String fichier = "graphe1.txt";
-        if(args.length > 0) {
+    public static void main(String[] args) {
+        String fichier = "";
+        if(args.length > 0) 
             fichier = args[0];
+        else {
+            System.err.println("Erreur : Veuillez spécifier un fichier à parser.");
+            return;
         }
-        Graphe g = new Graphe(fichier);
-        
-        Graphe.reduireDetteGraphe(g);
-        System.out.println("Graphe simplifié : ");
-        System.out.println("-----------------  ");
-        System.out.println(g.toString());
-        
-        ArrayList<ArrayList<String>> cc = Graphe.identifierCommunautes(g);
-        System.out.println("Identification des communautés : ");
-        System.out.println("-------------------------------  ");
-        for(int i = 0; i < cc.size(); i++) {
-            System.out.print("Communauté " + (i+1) + " : " + cc.get(i) + "\n");
+        try  {
+            Graphe g = new Graphe(fichier);
+            Graphe.reduireDetteGraphe(g);
+            System.out.println("+-----------------------------------+");
+            System.out.println("|          Graphe simplifie         |");
+            System.out.println("+===================================+");
+            System.out.println(g.toString());
+            System.out.println("");
+            
+            // Identification des communautés
+            ArrayList<ArrayList<String>> cc = Graphe.identifierCommunautes(g);
+            System.out.println("+-----------------------------------+");
+            System.out.println("|  Identification des communautes   |");
+            System.out.println("+===================================+");
+            System.out.println("| num.  |         Communaute        |");
+            System.out.println("+-------+---------------------------+");
+            for(int i = 0; i < cc.size(); i++)
+                System.out.format("%-2s%5d%-3s%25s%-2s%n", "|", i, " | ", 
+                                  cc.get(i), " |" );
+            System.out.println("+-----------------------------------+");
+            System.out.println("");
+            
+            // Identification des hubs sociaux
+            System.out.println("+-----------------------------------+");
+            System.out.println("| Identification des hubs sociaux   |");
+            System.out.println("+===================================+");
+            System.out.println("|   K   |            Hubs           |");
+            System.out.println("+-------+---------------------------+");
+            for(int i = 1; i <= 5; i++)
+                System.out.format("%-2s%5d%-3s%25s%-2s%n", "|", i, " | ", 
+                                  Graphe.hubSociaux(g, i), " |" );
+            System.out.println("+-----------------------------------+");
+            System.out.println("");
+            
+            System.out.println("Identification d'un des plus grands groupes "
+                    + "d'amis :");
+            System.out.println("---------------------------------------------------");
+            System.out.println(Graphe.identifierGrpMaxAmis(g));
+        } catch(IOException e) {
+            System.err.println("Erreur : Veuillez spécifier un fichier correct.");
         }
-        
-        System.out.println();
-        System.out.println("Identification hubs sociaux (min. 1 individus) :");
-        System.out.println("----------------------------------------------- ");
-        System.out.println("Hubs : " + Graphe.hubSociaux(g, 1) + "\n");
-        System.out.println("Identification hubs sociaux (min. 2 individus) :");
-        System.out.println("----------------------------------------------- ");
-        System.out.println("Hubs : " + Graphe.hubSociaux(g, 2) + "\n");
-        System.out.println("Identification hubs sociaux (min. 3 individus) :");
-        System.out.println("----------------------------------------------- ");
-        System.out.println("Hubs : " + Graphe.hubSociaux(g, 3) + "\n");
-        System.out.println("Identification d'un des plus grands groupes "
-                + "d'amis :");
-        System.out.println("---------------------------------------------------");
-        System.out.println(Graphe.identifierGrpMaxAmis(g));
-        
     }
 }
